64606998975521ec8df32947aa4039a981c799d5
Nikhil Idiculla <nikhil.idiculla@gmail.com>
2025-12-08 15:16:17 -0800

    zfw_sandbox refactor WIP

---
+++ modules/zfw_sandbox/zfw_sandbox/zfw_engine.py
@@ -0,0 +1,47 @@
+import sys
+
+import zfw_core
+
+
+class ZfwEngine:
+    def __init__(self, *, app_name: str, debug: bool, swapchain_image_count: int):
+        self.gpu_context = zfw_core.GpuContext(
+            app_name=app_name,
+            enable_debug_layer_support=debug,
+            enable_present_support=True,
+        )
+        self.window_context = zfw_core.WindowContext(gpu_context=self.gpu_context)
+        self.render_context = zfw_core.RendererContext(gpu_context=self.gpu_context)
+
+        # Create window, GPU surface:
+        self.window = self.window_context.create_window(
+            width=1280,
+            height=720,
+            title="Zero Sandbox",
+        )
+        self.surface = self.window.create_surface()
+
+        # Create GPU device using the surface:
+        physical_device = next(iter(self.gpu_context.enumerate_physical_devices()))
+        self.device = self.gpu_context.create_device(
+            physical_device=physical_device,
+            surface=self.surface,
+        )
+
+        # Create swapchain:
+        self.swapchain = self.device.create_swapchain(
+            surface=self.surface,
+            image_count=swapchain_image_count,
+        )
+
+        # Create renderer:
+        self.renderer = self.render_context.create_renderer(gpu_device=self.device)
+
+    def print_gpu_debug_info(
+        self,
+        file: zfw_core.SupportsWrite[str] = sys.stdout,
+    ):
+        print("<gpu-debug-info>")
+        self.gpu_context.print_debug_info(out=file)
+        print()
+        print("</gpu-debug-info>")
---
+++ modules/zfw_sandbox/zfw_sandbox/zfw_sandbox_cli.py
@@ -0,0 +1,49 @@
+import argparse
+import sys
+
+import zfw_core
+
+from .zfw_engine import ZfwEngine
+
+
+def main():
+    ap = argparse.ArgumentParser(description="Zero Sandbox Application")
+    ap.add_argument(
+        "--swapchain-image-count",
+        type=int,
+        default=3,
+        choices=[2, 3],
+        help="Number of swapchain images: 2 for double buffering, 3 for triple buffering",
+    )
+    ap.add_argument(
+        "--disable-vulkan-debug-layers",
+        dest="enable_vulkan_debug_layers",
+        action="store_false",
+        help="Disable Vulkan debug layers (enabled by default)",
+    )
+    args = ap.parse_args()
+
+    engine = ZfwEngine(
+        app_name="Zero Sandbox",
+        debug=args.enable_vulkan_debug_layers,
+        swapchain_image_count=args.swapchain_image_count,
+    )
+
+    while not engine.window.should_close():
+        engine.window.poll_events()
+
+        # TODO: render
+
+
+def print_gpu_debug_info(
+    gpu_context: zfw_core.GpuContext,
+    file: zfw_core.SupportsWrite[str] = sys.stdout,
+):
+    print("<gpu-debug-info>")
+    gpu_context.print_debug_info(out=file)
+    print()
+    print("</gpu-debug-info>")
+
+
+if __name__ == "__main__":
+    main()
