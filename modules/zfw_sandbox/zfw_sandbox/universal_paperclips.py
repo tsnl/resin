51004f26ab5e3a5e7b5ff41a9965cf7a0e5e6ff8
Nikhil Idiculla <nikhil.idiculla@gmail.com>
2025-12-18 12:50:51 -0800

    Store 'bg_image' on 'self'

--- modules/zfw_sandbox/zfw_sandbox/universal_paperclips.py
+++ modules/zfw_sandbox/zfw_sandbox/universal_paperclips.py
@@ -5,7 +5,7 @@ from .bundled_data import BUNDLED_DATA_PATH
 
 class UniversalPaperclipsWidget(zfw.GuiWidget):
     def __init__(self, engine: zfw.Engine):
-        bg_image = zfw.RendererImage(
+        self._bg_image = zfw.RendererImage(
             renderer=engine.renderer,
             data=zfw.load_rgba_image(
                 BUNDLED_DATA_PATH / "data/UniversalPaperclipsBackgrounds/hills.png"
@@ -17,7 +17,7 @@ class UniversalPaperclipsWidget(zfw.GuiWidget):
             grid_rows=(100, -1, -1),
             grid_cols=(-1, -1, -1),
             style_classes=["central"],
-            image=bg_image,
+            image=self._bg_image,
             theme={
                 "label": {
                     "bg_color": (0.0, 0.0, 0.0, 0.8),
