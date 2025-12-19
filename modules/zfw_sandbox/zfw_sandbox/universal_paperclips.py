88cb4aea950c1dae2c22dd4bc379a9504b444171
Nikhil Idiculla <nikhil.idiculla@gmail.com>
2025-12-18 23:28:50 -0800

    Paperclips UI WIP

--- modules/zfw_sandbox/zfw_sandbox/universal_paperclips.py
+++ modules/zfw_sandbox/zfw_sandbox/universal_paperclips.py
@@ -3,6 +3,11 @@ import zfw
 from .bundled_data import BUNDLED_DATA_PATH
 
 
+#
+# GUI
+#
+
+
 class UniversalPaperclipsMainMenuWidget(zfw.GuiWidget):
     def __init__(self, engine: zfw.Engine):
         super().__init__(
@@ -74,7 +79,7 @@ class UniversalPaperclipsWidget(zfw.GuiWidget):
     def __init__(self, engine: zfw.Engine):
         super().__init__(
             window=engine.window,
-            grid_rows=(200, 80, -1, -1),
+            grid_rows=(200, 40, 40, -1, -1),
             grid_cols=(-1, -1, -1),
             style_classes=["central"],
             theme=_THEME,
@@ -96,16 +101,29 @@ class UniversalPaperclipsWidget(zfw.GuiWidget):
             style_classes=["label", "h1", "paperclip-counter"],
             text="Paperclips: 0",
         )
+        self._make_paperclip_button_widget = zfw.GuiWidget(
+            parent_widget=self,
+            row=2,
+            col=0,
+            style_classes=["button"],
+            text="Make Paperclip",
+        )
 
-        self._business_module_widget = UniversalPaperclipsBusinessWidget(
+        self._business_module_widget = UniversalPaperclipsBusinessModuleWidget(
             engine=engine,
             parent=self,
-            row=2,
+            row=3,
             col=0,
         )
 
+        # TODO: manufacturing module at (4,0)
 
-class UniversalPaperclipsBusinessWidget(zfw.GuiWidget):
+        # TODO: computational resources module at (2, 1)
+
+        # TODO: projects module at (4, 1)
+
+
+class UniversalPaperclipsBusinessModuleWidget(zfw.GuiWidget):
     def __init__(
         self,
         engine: zfw.Engine,
@@ -115,8 +133,17 @@ class UniversalPaperclipsBusinessWidget(zfw.GuiWidget):
     ):
         super().__init__(
             parent_widget=parent,
-            grid_rows=(40, -1),
-            grid_cols=(-1,),
+            grid_rows=(
+                _MODULE_HEADER_SIZE_DIP,
+                30,  # available funds
+                30,  # unsold inventory
+                30,  # price per paperclip
+                30,  # public demand
+                30,  # (spacer)
+                30,  # marketing button + level
+                30,  # marketing upgrade cost
+            ),
+            grid_cols=(50, 50, -1),
             row=row,
             col=col,
             style_classes=["module"],
@@ -127,37 +154,112 @@ class UniversalPaperclipsBusinessWidget(zfw.GuiWidget):
             parent_widget=self,
             row=0,
             col=0,
+            col_span=3,
             style_classes=["label", "h2"],
             text="Business",
         )
+        self._available_funds_widget = zfw.GuiWidget(
+            parent_widget=self,
+            row=1,
+            col=0,
+            col_span=3,
+            style_classes=["label"],
+            text="Available Funds: $0.00",
+        )
+        self._unsold_inventory_widget = zfw.GuiWidget(
+            parent_widget=self,
+            row=2,
+            col=0,
+            col_span=3,
+            style_classes=["label"],
+            text="Unsold Inventory: 0 Paperclips",
+        )
+        self._price_per_paperclip_lower_button_widget = zfw.GuiWidget(
+            parent_widget=self,
+            row=3,
+            col=0,
+            style_classes=["button", "button-pair-left"],
+            text="lower",
+        )
+        self._price_per_paperclip_higher_button_widget = zfw.GuiWidget(
+            parent_widget=self,
+            row=3,
+            col=1,
+            style_classes=["button", "button-pair-right"],
+            text="raise",
+        )
+        self._price_per_paperclip_label_widget = zfw.GuiWidget(
+            parent_widget=self,
+            row=3,
+            col=2,
+            style_classes=["label"],
+            text="Price per Paperclip: $0.00",
+        )
+        self._public_demand_widget = zfw.GuiWidget(
+            parent_widget=self,
+            row=4,
+            col=0,
+            col_span=3,
+            style_classes=["label"],
+            text="Public Demand: 0%",
+        )
+        self._marketing_button_widget = zfw.GuiWidget(
+            parent_widget=self,
+            row=6,
+            col=0,
+            col_span=2,
+            style_classes=["button"],
+            text="Marketing",
+        )
+        self._marketing_level_widget = zfw.GuiWidget(
+            parent_widget=self,
+            row=6,
+            col=2,
+            style_classes=["label"],
+            text="Level: 1",
+        )
 
+
+_MODULE_HEADER_SIZE_DIP = 40
+_MARGIN = 4
 
 _THEME = {
+    "central": {
+        "bg_color": (1.0, 1.0, 1.0, 1.0),
+        "bg_hover_color": (1.0, 1.0, 1.0, 1.0),
+    },
     "module": {
         "margin": (10, 10, 10, 10),
         "padding": (10, 10, 10, 10),
     },
     "label": {
-        "bg_color": (0.1, 0.1, 0.1, 1.0),
-        "fg_color": (0.9, 0.9, 0.9, 1.0),
-        "font": "sans-serif",
-        "padding": (10, 10, 10, 10),
+        "bg_color": (1.0, 1.0, 1.0, 0.0),
+        "fg_color": (0.0, 0.0, 0.0, 1.0),
+        "font": "serif",
+        "font_size_dip": 18,
+        "padding": (2, 5, 2, 5),
         "text_horizontal_alignment": "left",
-        "text_vertical_alignment": "top",
+        "text_vertical_alignment": "middle",
+    },
+    "button": {
+        "padding": (0, 0, 0, 0),
+        "margin": (_MARGIN,) * 4,
+        "text_horizontal_alignment": "center",
+        "text_vertical_alignment": "middle",
     },
     "h1": {
-        "bg_color": (1.0, 1.0, 1.0, 1.0),
+        "bg_color": (1.0,) * 4,
         "fg_color": (0.0, 0.0, 0.0, 1.0),
         "font_size_dip": 28,
-        "font": "sans-serif",
+        "font": "serif",
         "text_horizontal_alignment": "center",
         "text_vertical_alignment": "middle",
     },
     "h2": {
-        "bg_color": (1.0, 1.0, 1.0, 1.0),
+        "bg_color": (1.0,) * 4,
         "fg_color": (0.0, 0.0, 0.0, 1.0),
         "font_size_dip": 22,
-        "font": "sans-serif",
+        "font": "serif",
         "border_color": (0.0, 0.0, 0.0, 1.0),
         "border_thickness": (0, 0, 2, 0),
         "padding": (5, 5, 5, 5),
@@ -171,5 +273,16 @@ _THEME = {
         "text_horizontal_alignment": "left",
         "text_vertical_alignment": "top",
         "wrap": True,
+        "bg_color": (0.0, 0.0, 0.0, 1.0),
+        "fg_color": (0.9, 0.9, 0.9, 1.0),
+        "margin": (_MARGIN,) * 4,
+        "border_color": (0.5, 0.5, 0.5, 1.0),
+        "border_thickness": (2, 2, 2, 2),
+    },
+    "button-pair-left": {
+        "margin": (_MARGIN, _MARGIN // 2, _MARGIN, _MARGIN),
+    },
+    "button-pair-right": {
+        "margin": (_MARGIN, _MARGIN, _MARGIN, _MARGIN // 2),
     },
 }
