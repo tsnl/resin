11a62d9140849785dff3d2c826e45b8efe1be487
Nikhil Idiculla <nikhil.idiculla@gmail.com>
2026-01-01 09:54:21 -0800

    Get simd_math to build

--- src/draw_3d.rs
+++ src/draw_3d.rs
@@ -1,1 +1,16 @@
+pub struct Draw3dRenderer {
+    device: wgpu::Device,
 
+    target_size_wh: [u16; 2],
+}
+impl Draw3dRenderer {
+    pub fn new() -> Self {
+        todo!()
+    }
+}
+
+pub struct Draw3dFrame {}
+
+pub struct Draw3dGeometry {}
+
+pub struct Draw3dMaterial {}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod draw_3d;
 mod gpu_util;
 
 pub use draw_2d::*;
+pub use draw_3d::*;
 pub use gpu_util::*;
 
 use std::{collections::HashMap, ops::Range, sync::Arc};
