25c8602463e660191ebaec1a690304551d63f58d
Nikhil Idiculla <nikhil.idiculla@gmail.com>
2026-06-20 18:45:19 -0700

    [resin] Add dsl.prelude convenience exports
    Re-export View, param, const, zeros, Tensor, trace, and grad_fn for
    `from resin.dsl.prelude import *` workflows.

---
+++ src/resin/dsl/prelude.py
@@ -0,0 +1,16 @@
+"""Convenience re-exports for typed Resin DSL programs."""
+
+from resin.dsl.dsl import View, const, param, zeros
+from resin.dsl.functional import trace
+from resin.dsl.types import Tensor
+from resin.grad.grad_fn import grad_fn
+
+__all__ = [
+    "View",
+    "const",
+    "grad_fn",
+    "param",
+    "Tensor",
+    "trace",
+    "zeros",
+]
