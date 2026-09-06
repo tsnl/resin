e0cb8e7ebc5d42adeddba1cdd58fc9d663e7af76
Nikhil Idiculla <nikhil.idiculla@gmail.com>
2026-09-06 14:31:23 -0700

    Add nodejs to shell.nix for tree-sitter-resin builds

--- shell.nix
+++ shell.nix
@@ -2,32 +2,56 @@ let
   pkgs = import <nixpkgs> { };
   lib = pkgs.lib;
   linux = pkgs.stdenv.hostPlatform.isLinux;
-  windowLibraries = with pkgs; lib.optionals linux [
-    libx11
-    libxcursor
-    libxi
-    libxinerama
-    libxrandr
-    libxkbcommon
-    wayland
-  ];
+  windowLibraries =
+    with pkgs;
+    lib.optionals linux [
+      libx11
+      libxcursor
+      libxi
+      libxinerama
+      libxrandr
+      libxkbcommon
+      wayland
+    ];
 in
-pkgs.mkShell ({
-  # Fortify requires optimization, but Resin's default C builds use -O0.
-  hardeningDisable = [ "fortify" ];
+pkgs.mkShell (
+  {
+    # Fortify requires optimization, but Resin's default C builds use -O0.
+    hardeningDisable = [ "fortify" ];
 
-  packages = with pkgs; [
-    rustup
-    shaderc
-  ] ++ lib.optionals linux (with pkgs; [
-    vulkan-tools
-    vulkan-validation-layers
-    renderdoc
-  ]);
+    packages =
+      with pkgs;
+      [
+        rustup
+        shaderc
+        nodejs_26
+      ]
+      ++ lib.optionals linux (
+        with pkgs;
+        [
+          vulkan-tools
+          vulkan-validation-layers
+          renderdoc
+        ]
+      );
 
-  nativeBuildInputs = with pkgs; [ cmake pkg-config ] ++ lib.optional linux wayland-scanner;
-  buildInputs = windowLibraries;
+    nativeBuildInputs =
+      with pkgs;
+      [
+        cmake
+        pkg-config
+      ]
+      ++ lib.optional linux wayland-scanner;
+    buildInputs = windowLibraries;
 
-} // lib.optionalAttrs linux {
-  LD_LIBRARY_PATH = lib.makeLibraryPath ([ pkgs.vulkan-loader pkgs.stdenv.cc.cc.lib ] ++ windowLibraries);
-})
+  }
+  // lib.optionalAttrs linux {
+    LD_LIBRARY_PATH = lib.makeLibraryPath (
+      [
+        pkgs.vulkan-loader
+        pkgs.stdenv.cc.cc.lib
+      ]
+      ++ windowLibraries
+    );
+  }
+)
